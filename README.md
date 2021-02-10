6927cc5e8a682cb15f42eba9fa897ad3e58abc1a
FrostyApeOne <farshaddashti@gmail.com>
2021-02-10 14:42:24 +0000

    Initial

---
+++ README.md
@@ -0,0 +1,68 @@
+# FrostyBot
+
+FrostyBot is a simple Python based bot which allows users to perform an automated checkout on Currys.co.uk website. 
+
+## Supported sites
+- Currys.co.uk
+
+## Installation
+
+1. Download and Install Python from the following link
+
+	https://www.python.org/downloads/
+
+3. Install Selenium and playsound, run the following commands
+
+	```
+	python -m pip install selenium
+	```
+	```
+	python -m pip install playsound
+	```
+
+
+## Configuration and settings
+
+1. Rename the **settings-template.json** to **settings.json**
+2. Edit and save the file
+
+-----
+**productUrl** : direct URL to the item you'd like to check and purchase
+
+**refreshTime** : Refresh interval in seconds to check the page regularly for stock
+
+**postcode** : Your delivery address postcode
+
+**loginEmail** & **loginPassword**  : Your Curry's username and password, make sure this file is only on your computer as it will have your password stored as plain text!
+
+**paypalPayment** : If True, it will open the PayPal page in the last stage of the checkout process! Otherwise it redirects the user to the Credit Card payment!
+
+The Card Payment section allows you to enter your card details and let the bot fill in the WorldPay card payment form for you! Please make sure the project folder and settings file are stored securely on your computer as it will contain your Credit Card and personal details in plain text!
+
+Note: If the **autoPurchase** is set to True, the bot clicks on the pay button and attempts to finalise your purchase!
+
+## Run the bot
+
+To run the bot double click on FrostyBot.bat file in the root of the project folder, alternatively you can run the application by executing the following command
+```
+	python frostyBot.py
+```
+Leave the bot running and make sure you computer's audio volume is loud so that you can hear the alarm if the bot find stock and goes to the payment page! Unless the autoPurchase is enabled in the setting the bot will only take the user to the payment gateway and it does not perform the purchase or finalize the order!
+
+## Important 
+
+Please make sure you have already saved a default delivery address in your Curry's account, the postcode needs to match the one you have set in the settings file!
+
+Please make sure the chrome window stays maximized! Do not click on the page or agree with the cookies warning!
+
+Please note, sometimes the Curry's server may respond slower than usual in that case the bot might fail to take you through the checkout! You can check the Chrome window or the console and make sure it is running!
+
+## Test
+
+Test the bot by giving it address of an item which is in stock, you can watch how it works and make sure it hits the last stage and take you to the payment gateway!
+Once confirmed that it works as expected please replace the URL with your GPU product URL!
+
+
+
+
+
---
+++ frostyBot.py
@@ -0,0 +1,192 @@
+#
+# FrostyBot v1.1.0 
+#
+import time
+from selenium import webdriver
+from playsound import playsound
+import os
+import json
+
+
+dirname = os.path.dirname(__file__)
+
+chromeFilePath = os.path.join(dirname, 'chromedriver','chromedriver.exe')
+alarmMp3 = os.path.join(dirname, 'assets','alarm.mp3')
+settingsFilePath = os.path.join(dirname, 'settings.json')
+
+with open(settingsFilePath, 'r') as settingsFile:
+    settings = settingsFile.read()
+
+settingsObj = json.loads(settings)
+
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument("--incognito")
+
+browser = webdriver.Chrome(chromeFilePath, chrome_options=chrome_options)
+browser.get(settingsObj['productUrl'])
+browser.maximize_window()
+buyButton = False
+
+
+def checkStock():
+        failed_A = False
+        failed_B = False
+        try: 
+                addToCartBtn = browser.find_element_by_class_name("prd-oos")
+        except:
+                failed_A = True
+        try: 
+                addToCartBtn = browser.find_element_by_class_name("oos")
+        except:
+                failed_B = True
+
+        if (failed_A) and (failed_B):
+                raise Exception("InStock")
+
+
+def addToBasket():
+        try: 
+                parent_el = browser.find_element_by_xpath("//div[@id='product-actions']/div[@class='channels space-b'][@data-component='channels-panel']/div[@data-component='add-to-basket-button-wrapper']/button[1]")
+                parent_el.click()
+        except:
+                time.sleep(3)
+                addToBasket()
+
+def contToBasket():
+        try:         
+                parent_el2 = browser.find_element_by_xpath("//button[@data-interaction='Continue to basket']")
+                parent_el2.click()
+        except:
+                time.sleep(3)
+                contToBasket()
+
+def orderSum():
+        try:
+                parent_el3 = browser.find_element_by_xpath("//div[@data-component='StickyInner']/div[@data-component='OrderSummary']/button[1]")
+                parent_el3.click()
+        except:
+                time.sleep(3)
+                orderSum()
+
+def enterPostcode():
+        try:
+                searchPostcode = browser.find_element_by_xpath("//input[@placeholder='Enter town or postcode']")
+                searchPostcode.send_keys(settingsObj['postcode'])                
+        except:
+                time.sleep(3)
+                enterPostcode()
+
+def submitSearch():
+        try:
+                parent_el4 = browser.find_element_by_xpath("//button[@aria-label='Submit Search']")
+                parent_el4.click()                    
+        except:
+                time.sleep(3)    
+                submitSearch()
+
+def delivery():
+        try:
+                parent_el5 = browser.find_element_by_xpath("//div[@data-element='DeliverySlotBlock']/button[1]")
+                parent_el5.click()                 
+        except:
+                time.sleep(3)      
+                delivery()
+
+def enterEmail():
+        try:
+                email = browser.find_element_by_xpath("//form[@data-di-form-id='email']/div/div/input[@data-qa='CustomInput_email']")
+                email.send_keys(settingsObj['loginEmail'])
+                emailSubmit = browser.find_element_by_xpath("//form[@data-di-form-id='email']/button[1]")
+                emailSubmit.click()                
+        except:
+                time.sleep(3)      
+                enterEmail()
+
+def enterPassword():
+        try:
+                password = browser.find_element_by_xpath("//form[@id='password']/div/input[1]")
+                password.send_keys(settingsObj['loginPassword'])
+                passwordButton = browser.find_element_by_xpath("//form[@id='password']/div/button[1]")
+                passwordButton.click()                 
+        except:
+                time.sleep(3)    
+                enterPassword()
+
+def paypalPaymentM():
+        try:
+                paypal = browser.find_element_by_xpath("//div[@data-component='PaymentMethods']/div[@data-element='PaymentMethodsButtons']/div[@data-component='PayPalPayment']/button[1]")
+                paypal.click()                
+        except:
+                time.sleep(3)     
+                paypalPaymentM()
+
+def cardlPaymentM():
+        try:
+                time.sleep(1)
+                card = browser.find_element_by_xpath("//div[@data-component='PaymentMethods']/div[@data-element='PaymentMethodsButtons']/div[@data-component='CardPayment']/button[1]")
+                card.click() 
+
+                time.sleep(10)
+
+                cardNumber = browser.find_element_by_id("cardNumber")
+                cardNumber.send_keys(settingsObj['cardNumberCC'])
+
+                cardHolderName = browser.find_element_by_id("cardholderName")
+                cardHolderName.send_keys(settingsObj['cardHolderNameCC'])
+
+                cardExpiryMonth = browser.find_element_by_id("expiryMonth")
+                cardExpiryMonth.send_keys(settingsObj['expiryMonthCC'])
+
+                cardExpiryYear = browser.find_element_by_id("expiryYear")
+                cardExpiryYear.send_keys(settingsObj['expiryYearCC'])
+
+                cardCVC = browser.find_element_by_id("securityCode")
+                cardCVC.send_keys(settingsObj['cardCvc'])
+
+                if settingsObj['autoPurchase']:
+                        submitButton = browser.find_element_by_id("submitButton")
+                        submitButton.click()                
+
+
+        except:
+                time.sleep(3)      
+                cardlPaymentM()
+
+
+while not buyButton:
+
+    try:
+
+        checkStock()
+        print("Out of Stock........")
+
+        time.sleep(settingsObj['refreshTime'])
+        browser.refresh()
+
+    except:
+
+        acceptCookies = addButton = browser.find_element_by_id("onetrust-accept-btn-handler")
+        acceptCookies.click()
+
+        addToBasket()
+        time.sleep(2)
+        contToBasket()
+        time.sleep(2)
+        orderSum()
+        time.sleep(1)
+        enterPostcode()
+        time.sleep(1)
+        submitSearch()
+        time.sleep(2)
+        delivery()
+        time.sleep(1)
+        enterEmail()
+        time.sleep(3)
+        enterPassword()
+        time.sleep(3)
+        if settingsObj['paypalPayment']:
+            paypalPaymentM()
+        else:
+            cardlPaymentM()
+        playsound(alarmMp3)        
+        buyButton = True
